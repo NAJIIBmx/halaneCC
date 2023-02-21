aa086d18f379a63d7651d9ea80959ccd64ee150a
NAJIIBmx <125672741+NAJIIBmx@users.noreply.github.com>
2023-02-21 09:53:33 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,3 @@
+function myfunction(){
+alert(" am sorry its under development soon it will work but now only for testing purpose by the way to navigate through the website use navigation  bar  ");
+}
